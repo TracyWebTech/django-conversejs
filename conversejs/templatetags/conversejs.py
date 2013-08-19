80d48451e6fb55dfd91e74a80fd4d262bd79f018
Sergio Oliveira <sergio@tracy.com.br>
2013-08-19 13:30:56 -0300

    Configuring initialize from django settings

--- conversejs/templatetags/conversejs.py
+++ conversejs/templatetags/conversejs.py
@@ -1,21 +1,30 @@
 
 from django import template
 
+from ..utils import get_conversejs_settings
+
 register = template.Library()
 TEMPLATE_PATH = 'conversejs/includes/'
 
+
 @register.inclusion_tag(TEMPLATE_PATH + 'initialize.html', takes_context=True)
 def conversejs_initialize(context):
-    request = context.get('request')
-    return locals()
+    tag_context = {
+        'request': context.get('request'),
+    }
+
+    tag_context.update(get_conversejs_settings())
+    return tag_context
+
 
 @register.inclusion_tag(TEMPLATE_PATH + 'chatpanel.html', takes_context=True)
 def conversejs_chatpanel(context):
-    request = context.get('request')
-    return locals()
+    return { 'request': context.get('request'), }
+
 
 @register.inclusion_tag(TEMPLATE_PATH + 'static.html', takes_context=True)
 def conversejs_static(context):
-    request = context.get('request')
-    STATIC_URL = context.get('STATIC_URL')
-    return locals()
+    return {
+        'request': context.get('request'),
+        'STATIC_URL': context.get('STATIC_URL'),
+    }
---
+++ conversejs/utils.py
@@ -0,0 +1,24 @@
+
+from django.conf import settings
+
+
+def get_conversejs_settings():
+    converse_settings = {
+        'CONVERSEJS_AUTO_LIST_ROOMS': False,
+        'CONVERSEJS_AUTO_SUBSCRIBE': False,
+        'CONVERSEJS_BOSH_SERVICE_URL': 'https://bind.opkode.im',
+        'CONVERSEJS_HIDE_MUC_SERVER': False,
+        'CONVERSEJS_PREBIND': True,
+        'CONVERSEJS_SHOW_CONTROLBOX_BY_DEFAULT': True,
+        'CONVERSEJS_XHR_USER_SEARCH': False,
+    }
+    
+    for key, value in converse_settings.items():
+        conf = getattr(settings, key, value)
+
+        if isinstance(conf, bool):
+            conf = unicode(conf).lower()
+        
+        converse_settings[key] = conf
+
+    return converse_settings
