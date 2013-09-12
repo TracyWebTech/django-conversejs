32673745d7a0416520cbc762da3fac239420d4e5
Sergio Oliveira <sergio@tracy.com.br>
2013-09-12 11:07:26 -0300

    Bumping version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ package_data_globs = [
 setup(
     name='django-conversejs',
     description='Adds converse.js (javascript XMPP client) to Django',
-    version='0.2.3',
+    version='0.2.4',
     long_description=read('README.rst'),
     packages=['conversejs',
               'conversejs.migrations',
