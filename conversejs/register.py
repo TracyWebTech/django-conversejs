7178131634c6b104e4dde04f27e313911cabce0e
Sergio Oliveira <sergio@tracy.com.br>
2013-09-12 10:46:33 -0300

    Adding copyright notes

--- conversejs/register.py
+++ conversejs/register.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
+    Inicial version taken from:
+
     SleekXMPP: The Sleek XMPP Library
     Copyright (C) 2010  Nathanael C. Fritz
-    This file is part of SleekXMPP.
 
-    See the file LICENSE for copying permission.
+    Modifications by django-conversejs authors.
+
 """
 
 import sys
