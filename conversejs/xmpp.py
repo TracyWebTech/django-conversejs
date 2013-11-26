df51d7870431e288b4c501aee1db519f95232776
Luan <luanpab@gmail.com>
2013-11-26 09:03:53 -0200

    More refactoring of xmpp module

--- conversejs/xmpp.py
+++ conversejs/xmpp.py
@@ -32,7 +32,7 @@ if sys.version_info < (3, 0):
     setdefaultencoding('utf8')
 
 
-TIMEOUT = 10
+TIMEOUT = 30
 
 
 def register(client, username, password, name, email):
@@ -42,11 +42,11 @@ def register(client, username, password, name, email):
     iq['register']['password'] = password
     iq['register']['name'] = name
     iq['register']['email'] = email
-    iq.send(now=True) #, timeout=TIMEOUT)
+    iq.send(now=True, timeout=TIMEOUT)
 
 
 def registration_wrapper(client, function, logger_success, logger_error,
-                         use_client, args, kwargs):
+                         args, kwargs):
 
     connected = client.connect(reattempt=False)
 
@@ -58,16 +58,14 @@ def registration_wrapper(client, function, logger_success, logger_error,
 
     success = False
     try:
-        if use_client:
-            function(client, *args, **kwargs)
-        else:
-            function(*args, **kwargs)
-        logger.info(logger_success % client.boundjid)
-        success = True
+        function(*args, **kwargs)
     except IqError as e:
-        logger.error(logger_error % e.iq['error']['text'])
+        logger.error(logger_error, client.boundjid.bare)
     except IqTimeout:
         logger.error("No response from server.")
+    else:
+        logger.info(logger_success, client.boundjid)
+        success = True
     finally:
         client.disconnect()
     return success
@@ -77,13 +75,12 @@ def register_account(jid, password, name='', email=''):
     client = sleekxmpp.ClientXMPP(jid, password)
     client.register_plugin('xep_0077') # In-band Registration
 
-    registration_wrapper(
+    return registration_wrapper(
         client=client,
         function=register,
         logger_success="Account created for %s!",
         logger_error="Could not register account: %s",
-        use_client=True,
-        args=[client.boundjid.user, password, name, email],
+        args=(client, client.boundjid.user, password, name, email),
         kwargs={}
     )
 
@@ -92,12 +89,11 @@ def change_password(jid, old_password, new_password):
     client = sleekxmpp.ClientXMPP(jid, old_password)
     client.register_plugin('xep_0077') # In-band Registration
 
-    registration_wrapper(
+    return registration_wrapper(
         client=client,
         function=client['xep_0077'].change_password,
         logger_success="Password changed for %s!",
         logger_error="Could not change password for account: %s",
-        use_client=False,
-        args=[new_password, client.boundjid.server, client.boundjid.bare],
+        args=(new_password, client.boundjid.server, client.boundjid.bare),
         kwargs=dict(timeout=TIMEOUT)
     )
